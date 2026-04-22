8e368d7bd995f7b3b658cb190ee2a59bc5417076
D3lmaschio <matheus.rubens.ti@gmail.com>
2026-04-22 18:45:38 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CODE
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// This is a simple example of using the Console.WriteLine method.
+        /// </summary>
+        static void Main(string[] args)
+        {   
+            var text = "Hello, World!";
+            Console.WriteLine(text);
+
+            string text2 = "teste";
+            var num = 2324m;
+            var character = '0';
+        
+            
+            Console.WriteLine(num.GetType());
+            Console.WriteLine(num);
+
+
+            
+        }
+    }
+}
+
+
